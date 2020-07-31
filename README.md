e8293636ceb20963e20d4a98baf8d86b893dc64f
Gururaj Allagi <68994567+Gururaj-Allagi@users.noreply.github.com>
2020-07-31 15:15:46 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Python_Practice_FB
+![logo](https://rohandas28.github.io/img/favicon.png) 
+# Automatic Facebook Login With Python
+---
+---
+#### This is a Python script to automate the login process on Facebook.com. It uses Python 3.x and  Selenium library to automate the process.
+---
+---
+## ```Installation```
+### 1. Install Selenium using "PIP" ``` pip install selenium or pip3 install selenium```.
+### 2. Install Webdriver Manager Using "PIP" ```pip install webdriver_manager or pip3 install webdriver_manager ```
+### 3. Run Main.py.
+---
+---
+# ```Exception```
+#### If your drivers(ChromeDriverManager or GeckoDriverManager) are not installed then You need to have a proper Internet to install the required drivers.
+---
+---
+# ```Contributors```
+## 1. [Rohan Das](https://rohandas28.github.io/).
+## 2. [Nikhil Raj Pandey](https://github.com/NikhilRajPandey).
+---
+---
---
+++ main.py
@@ -0,0 +1,44 @@
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager 
+from webdriver_manager.firefox import GeckoDriverManager
+
+# Initiliaze Webdriver
+try:
+	driver = webdriver.Chrome(ChromeDriverManager().install())
+except:
+	driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+
+def FacebookLogin():
+    #Enter Your Email ID And Password
+	user=input('Enter Email Id:')  
+	password=input('Enter Password:')
+
+	#Opening Facebook.
+	driver.get('https://www.facebook.com/') 
+	print ("Facebook Opened") 
+	time.sleep(1) 
+	  
+    #Entering Email and Password
+	username_box = driver.find_element_by_id('email') 
+	username_box.send_keys(user) 
+	print ("Email Id entered") 
+	time.sleep(1) 
+	  
+	password_box = driver.find_element_by_id('pass') 
+	password_box.send_keys(password) 
+	print ("Password entered") 
+
+	#Pressing The Login Button  
+	login_box = driver.find_element_by_id('loginbutton') 
+	login_box.click() 
+
+	  
+	print ("Done") 
+	input('Press anything to quit') 
+	driver.quit() 
+	print("Finished")
+
+FacebookLogin()
+
+
